b91976bd904ca422742313cc95196883fb831663
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-02-06 19:18:03 +0300

    Update expensesController.js

--- backend/controllers/expensesController.js
+++ backend/controllers/expensesController.js
@@ -3,12 +3,12 @@ const Expense = require("../model/Expense");
 const getUserExpenses = async (req, res) => {
   const { user } = req.body;
   if (!user) return res.status(400).json({ message: "User login required" });
-
+  console.log(user);
   const expenses = await Expense.find({ username: req.body.user }).exec();
   if (!expenses) {
-    return res.status(204).json({ message: `User ${req.params.id} not found` });
+    return res.status(204).json({ message: `User ${user} not found` });
   }
-  //   console.log(expenses);
+  console.log(expenses);
   res.json(expenses);
 };
 

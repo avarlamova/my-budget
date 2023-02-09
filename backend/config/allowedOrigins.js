362656e7778889b93fe62029cccbfb97971f1400
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-02-09 17:58:47 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3001", //dev
   "https://brand-new-budget.onrender.com/", //prod
+  "https://brand-new-budget-api.onrender.com",
 ];
 
 module.exports = allowedOrigins;

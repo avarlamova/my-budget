03f88325ea46fe264be47dbddc495b047530c146
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-02-09 18:49:07 +0300

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,6 +1,9 @@
 const allowedOrigins = require("./allowedOrigins");
+console.log(allowedOrigins);
 const corsOptions = {
   origin: (origin, callback) => {
+    console.log("origin", origin);
+    console.log(allowedOrigins.indexOf(origin));
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
